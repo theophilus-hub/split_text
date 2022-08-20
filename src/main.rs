a19a12e7be4019a061254f98f3d90ce33483a837
tarri9192 <tarri9192@gmail.com>
2022-08-20 03:07:15 +0100

    real

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use my_text_lib::{wordify, breakify};
 
 
 fn main() {
-    let text = String::from("where can i see my broken lines in this sentence");
+
+    let text = String::from("where can i see my broken lines in this manually generated sentence");
     let words: Vec<String> = wordify(text);
-    let vec = vec![4,2];
+    let line_breaks: Vec<i32> = vec![4,3,2,1];
+    let result:Vec<String> = breakify(words, line_breaks );
 
-    println!("{:?}", breakify(words, vec ));
-
-
+    println!("{:?}", result);
 
 }
